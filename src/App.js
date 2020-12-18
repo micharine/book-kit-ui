998442e6bf22f55b2d43d21d86d9044e3dd25f52
Michelle Hansey <6622259+micharine@users.noreply.github.com>
2020-12-17 17:12:40 -0800

    wip: added stripe card element, addToCart with more information, some styling stuff

--- src/App.js
+++ src/App.js
@@ -1,88 +1,156 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useQuery } from '@apollo/react-hooks';
+import React, { useState } from 'react'
+import './App.css'
+import { useQuery } from '@apollo/react-hooks'
 import {
     GET_INVENTORY,
     GET_ONE_INVENTORY_ITEM,
     EDIT_INVENTORY_ITEM,
-} from './queries';
+} from './queries'
 import {
     Button,
-    ButtonGroup,
     Card,
     CardBody,
     CardHeader,
+    CardImg,
+    CardText,
+    CardTitle,
+    CardSubtitle,
     Spinner,
-} from 'reactstrap';
+} from 'reactstrap'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+import { CardElement } from '@stripe/react-stripe-js'
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+// TODO: Move Test Key to config file
+const stripePromise = loadStripe(
+    'pk_test_51HzKckFlyo65azYfF87FppYlDzQq7FXgNMZl7NLSSyJ9HMCY5i8iaRXRPsLNfs85os9lAUEANSQYtjZl00Yzdr6700grHrmrKo'
+)
+
 function App() {
-    const fetchInventoryItems = useQuery(GET_INVENTORY);
+    let [itemsInCart, setItemsInCart] = useState({})
+    let [totalCost, setTotalCost] = useState(0);
+    const fetchInventoryItems = useQuery(GET_INVENTORY)
     const getOneInventoryItem = useQuery(GET_ONE_INVENTORY_ITEM, {
         variables: { id: 1 },
-    });
+    })
 
     // let inventoryItems = fetchInventoryItems.data.getInventoryItems;
-    const [cSelected, setCSelected] = useState([]);
 
     if (fetchInventoryItems.loading || getOneInventoryItem.loading)
-        return <Spinner color="dark" />;
+        return <Spinner color="dark" />
     if (fetchInventoryItems.error || getOneInventoryItem.error) {
-        console.log(fetchInventoryItems.error);
-        console.log(getOneInventoryItem.error);
-        return <React.Fragment> Error</React.Fragment>;
+        console.log(fetchInventoryItems.error)
+        console.log(getOneInventoryItem.error)
+        return <React.Fragment> Error</React.Fragment>
     }
 
-    const onCheckboxBtnClick = (selected) => {
-        const index = cSelected.indexOf(selected);
-        if (index < 0) {
-            cSelected.push(selected);
+    const onAddToCartClick = ({id, name, cost, code}) => {
+        let editedCart = {...itemsInCart};
+
+        if (!itemsInCart[id]) {
+            editedCart[id] = {
+                    name: name,
+                    cost: cost? cost : 0,
+                    code: code,
+            }
         } else {
-            cSelected.splice(index, 1);
+            delete editedCart[id];
         }
-        setCSelected([...cSelected]);
-    };
-// TODO: 
-//  Add Checkout (Stripe)
-//  Add Cart
-//  Update Inventory on Checkout
-//  Create order on Checkout
+
+        setItemsInCart(editedCart)
+    }
+    // TODO:
+    //  Add Checkout (Stripe)
+    //  Add Cart
+    //  Update Inventory on Checkout
+    //  Create order on Checkout
+    //  IMAGES?
 
     // const updateInventoryItem = useQuery(EDIT_INVENTORY_ITEM, { variables: { id: 9, quantityInStock: 5 }});
     return (
-        <div className="App">
-            <div className="container">
-                <Card>
-                    <CardHeader>Bible Book Club Kits</CardHeader>
-                    <CardBody>
-                          <div>
+        <Elements stripe={stripePromise}>
+            <div className="App">
+                <div className="container">
+                    <Card>
+                        <CardHeader>Bible Book Club Kits</CardHeader>
+                        <CardBody>
+                            <div>
                                 {fetchInventoryItems.data.getInventoryItems.map(
                                     (item) => {
                                         return (
                                             <div key={item.id}>
-                                                <p >{item.name}</p>
-                                                <Button
-                                                    color="primary"
-                                                    onClick={() =>
-                                                        onCheckboxBtnClick(item.id)
-                                                    }
-                                                    active={cSelected.includes(
-                                                        item.id
-                                                    )}
-                                                >
-                                                    Add to Cart
-                                                </Button>
+                                                <Card>
+                                                    <CardImg
+                                                        top
+                                                        width="100%"
+                                                        src="/assets/318x180.svg"
+                                                        alt={'Image for '+item.name+ ' book club kit'}
+                                                    />
+                                                    <CardBody>
+                                                        <CardTitle tag="h5">
+                                                            {item.name}
+                                                        </CardTitle>
+                                                        <CardSubtitle
+                                                            tag="h6"
+                                                            className="mb-2 text-muted"
+                                                        >
+                                                            {item.cost
+                                                                ? '$' +
+                                                                  item.cost
+                                                                : 'FREE'}
+                                                        </CardSubtitle>
+                                                        <CardText>
+                                                            {item.description}
+                                                        </CardText>
+                                                        <Button
+                                                            color="primary"
+                                                            onClick={() =>
+                                                                onAddToCartClick(
+                                                                    item
+                                                                )
+                                                            }
+                                                            active={!!itemsInCart[item.id]}
+                                                        >
+                                                            Add to Cart
+                                                        </Button>
+                                                    </CardBody>
+                                                </Card>
                                             </div>
-                                        );
+                                        )
                                     }
                                 )}
-                            <p>Selected: {JSON.stringify(cSelected)}</p>
+                            </div>
+                        </CardBody>
+                    </Card>
+                    <Card>
+                        <CardHeader>Your Cart</CardHeader>
+                        <CardBody>
+                            <p>Selected: {JSON.stringify(itemsInCart)}</p>
+                            <CardElement
+                                options={{
+                                    style: {
+                                        base: {
+                                            fontSize: '16px',
+                                            color: '#424770',
+                                            '::placeholder': {
+                                                color: '#aab7c4',
+                                            },
+                                        },
+                                        invalid: {
+                                            color: '#9e2146',
+                                        },
+                                    },
+                                }}
+                            />
                             <Button color="success">Checkout</Button>{' '}
-                        </div>
-                    </CardBody>
-                </Card>
+                        </CardBody>
+                    </Card>
+                </div>
             </div>
-        </div>
-    );
+        </Elements>
+    )
 }
 
-export default App;
+export default App
