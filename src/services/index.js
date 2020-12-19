b620cdd56abe087fc0635da7e8d927f64161d5d9
Michelle Hansey <6622259+micharine@users.noreply.github.com>
2020-12-18 17:01:23 -0800

    wip: post method to create payment intent and get client secret for confirm payment

---
+++ src/services/index.js
@@ -0,0 +1,43 @@
+// STRIPE
+
+let apiEndpoint = 'http://localhost:4000';
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+  
+  let createPaymentIntent = ({items,currency})=>{
+    let url = apiEndpoint+'/create-payment-intent';
+    postData(url, { items, currency })
+    .then(data => {
+      console.log(data); // JSON data parsed by `data.json()` call
+      // Return the client secret for confirm payment call
+      return(data.client_secret);
+    });
+  }
+
+  let confirmPayment = (clientSecret)=>{
+    // call stripe.confirmCardPayment with the client secret
+  }
+  
+
+var response = fetch('/secret').then(function(response) {
+    return response.json();
+  }).then(function(responseJson) {
+    var clientSecret = responseJson.client_secret;
+    // Call stripe.confirmCardPayment() with the client secret.
+  });
